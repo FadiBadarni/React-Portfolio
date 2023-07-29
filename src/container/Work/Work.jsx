b85e16f11d4557a7812318d7fc4acf0145838814
Fadi <fadybd1@gmail.com>
2023-07-29 14:08:52 +0300

    projects sort

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -14,7 +14,6 @@ const projects = [
     imgUrl: images.warehouse,
     repository: "https://github.com/FadiBadarni/V-Warehouse",
     tags: ["Spring", "React"],
-    featured: true,
   },
   {
     title: "Compiler",
@@ -69,10 +68,6 @@ const projects = [
 const Work = () => {
   const [animateCard] = useState({ y: 0, opacity: 1 });
 
-  const featuredProject = projects.find((project) => project.featured);
-  const startDate = "March 14, 2023";
-  // Filter out the featured project from the other projects
-  const otherProjects = projects.filter((project) => !project.featured);
   return (
     <>
       <h2 className="head-text">
@@ -80,91 +75,13 @@ const Work = () => {
         <br />
       </h2>
 
-      {/* Render the featured project */}
-      <motion.div
-        animate={animateCard}
-        transition={{ duration: 0.5, delayChildren: 0.5 }}
-        className="app__work-portfolio app__work-featured"
-      >
-        <div className={`app__work-item app__flex featured`}>
-          <div className="app__work-img app__flex">
-            <img src={featuredProject.imgUrl} alt={featuredProject.title} />
-
-            <motion.div
-              whileHover={{ opacity: [0, 1] }}
-              transition={{
-                duration: 0.25,
-                ease: "easeInOut",
-                staggerChildren: 0.5,
-              }}
-              className="app__work-hover app__flex"
-            >
-              <a
-                href={featuredProject.repository}
-                target="_blank"
-                rel="noreferrer"
-              >
-                <motion.div
-                  whileInView={{ scale: [0, 1] }}
-                  whileHover={{ scale: [1, 0.9] }}
-                  transition={{ duration: 0.25 }}
-                  className="app__flex"
-                >
-                  <AiFillGithub />
-                </motion.div>
-              </a>
-            </motion.div>
-          </div>
-
-          <div className="app__work-content app__flex">
-            <h4 className="bold-text">{featuredProject.title}</h4>
-            <p className="p-text" style={{ marginTop: 10 }}>
-              {featuredProject.description}
-            </p>
-            <div className="app__work-tag app__flex">
-              {featuredProject.tags.map((tag, index) => (
-                <p
-                  key={`tag-${index}`}
-                  className="p-text app__work-tag-box"
-                  style={{ fontWeight: "bold" }}
-                >
-                  {tag}
-                </p>
-              ))}
-            </div>
-          </div>
-        </div>
-        <div className="app__work-featured-text">
-          <p style={{ color: "red" }}>
-            Currently Developing - {featuredProject.title}
-          </p>
-          <p>Start date - {startDate}</p>
-          <p>Technologies used - {featuredProject.tags.join(", ")}</p>
-          <p>
-            Virtual Warehouse is a web application designed to manage and track
-            equipment in a visual communication department's virtual warehouse.
-            The application is built using Java Spring Boot for the backend and
-            React JS for the frontend, with styling provided by CSS combined
-            with SCSS (Sass).
-          </p>
-          <a
-            href={featuredProject.repository}
-            target="_blank"
-            rel="noreferrer"
-            style={{ textDecoration: "underline" }}
-          >
-            Visit the GitHub repository for more information
-          </a>
-        </div>
-      </motion.div>
-
-      {/* Render the other projects */}
+      {/* Render the projects */}
       <motion.div
         animate={animateCard}
         transition={{ duration: 0.5, delayChildren: 0.5 }}
         className="app__work-portfolio"
       >
-        {otherProjects.map((project, index) => (
+        {projects.map((project, index) => (
           <div className="app__work-item app__flex" key={index}>
             <div className="app__work-img app__flex">
               <img src={project.imgUrl} alt={project.title} />
